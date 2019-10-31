3b7b6c8d496166ceb28c64d244b0655d14bba17d
kerinlin <18296613547@qq.com>
2019-10-31 18:37:28 +0800

    Fix the text overflow bug

--- browser/app/js/browser/BrowserDropdown.js
+++ browser/app/js/browser/BrowserDropdown.js
@@ -97,8 +97,8 @@ export class BrowserDropdown extends React.Component {
           </Dropdown.Toggle>
           <Dropdown.Menu className="dropdown-menu-right">
             <li>
-              <a target="_blank" href="https://github.com/minio/minio">
-                GitHub <i className="fab fa-github" />
+              <a target="_blank" href="https://github.com/LambdaIM">
+                GitHub <i className="fa fa-github" />
               </a>
             </li>
             <li>
@@ -107,7 +107,7 @@ export class BrowserDropdown extends React.Component {
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://docs.min.io/">
+              <a target="_blank" href="https://github.com/LambdaIM/launch">
               <Trans>doc</Trans>  <i className="fa fa-book" />
               </a>
             </li>
